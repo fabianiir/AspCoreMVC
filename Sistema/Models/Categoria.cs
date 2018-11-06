0a3b9c29bff1bca07638548435795ce46d56f33d
fabian <niir.rff2@gmail.com>
2018-11-06 15:36:37 -0600

    Tabla CRUD
    Finalizacion de la tabla con paginacion y filtro

--- Sistema/Models/Categoria.cs
+++ Sistema/Models/Categoria.cs
@@ -9,12 +9,15 @@ namespace Sistema.Models
     public class Categoria
     {
         public int CategoriaID { get; set; }
+
         [Required(ErrorMessage ="Campo requerido")]
         [StringLength(50, MinimumLength =3, ErrorMessage ="El nombre debe tener de 3 a 5 caracteres")]
         public string Nombre { get; set; }
+
         [StringLength(256, ErrorMessage ="La descripcion no debe exceder los 256 caracteres")]
         [Display(Name ="Descripción")]
         public string Descripcion { get; set; }
+
         public Boolean Estado { get; set; }
 
 
